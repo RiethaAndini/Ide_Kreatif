4555836368aa61dac2a940cac748f1d311e34acd
RiethaAndini <andinirietha@gmail.com>
2025-02-16 22:39:10 +0700

    menambahkan kode kedalam init_session

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-
+//ambil data dari sesi
+$userId = $_SESSION["user_id"];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 // Ambil notifikasi jika ada, kemudian hapus dari sesi
@@ -8,11 +9,13 @@ $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
 }
+/* periksa apakah sesi username dan role suda ada,
+jika tidak arahkan ke halaman login */
 if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
     $_SESSION['notification'] = [
         'type' => 'danger',
         'message' => 'Silakan Login Terlebih Dahulu!'
     ];
     header('Location: ./auth/login.php');
-    exit();
+    exit(); // pastikan script berhenti setelan pengalihan
 }
